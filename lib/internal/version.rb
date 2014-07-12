897db987651835266826eac8314e7ef5cab1931d
Daniel da Silva Ferreira (dsferreira) <daniel.ferreira@dsferreira.com>
2014-07-12 22:37:52 +0000

    Version
    Update version definition.

--- lib/internal/version.rb
+++ lib/internal/version.rb
@@ -1,37 +1,33 @@
 module Internal
   def self.version
-    critical = 0 # Broken backwards compatibility of public interface
-                 # Production code based on previous releases maybe be broken by this release
-    major = 1 # Changed public interface. No backwards compatibility issues.
+    # Any third party software should rely only on current major version
+    # Any release under the current major release should be good.
+    # Use: gem 'internal', '~> 1.0'
+    #
+    # Any extension to the current gem should rely only on current major and minor release.
+    # A Change to the major or minor release can brake the extension
+    # Usage: gem 'internal', '~> 1.1.0'
+    major = 0 # Broken backwards compatibility of public interface
+              # Production code based on previous releases maybe be broken by this release
+    minor = 1 # Changed public interface. No backwards compatibility issues.
               # Production code based on previous releases should not present any issues after upgrade.
               # Production code based on this release may be broken when using previous releases.
-    minor = 0 # Changed internal interface
+              # Changes to internal interface with broken backwards compatibility
+              # Library extensions may be broken 
+    tiny = 0 # Changed internal interface with backward compatibility
               # There should be no impact on third party software
-              # Library extensions using library internal interface may be broken. 
-    patch = nil # Changed private interface (protected and/or private methods). Bug fixes. Refactoring. 
-                # Patch needs to increase for each commit. Patch is a three digits figure.
-                # No side effects of any sort should be detected
-    alpha = 2 # First phase of development of new major or critical release
-    beta = nil # release candidate for first critical release (v0.9.0, v1.0.0.0beta1, v1.0.0)
-    pre_release = nil # release candidate of new minor releases
-    release_candidate = nil # Used before releasing new major or critical releases
+              # Library extensions using library internal interface are not broken. 
+              # Changed private interface (protected and/or private methods). Bug fixes. Refactoring. 
+              # No side effects of any sort should be detected
+    alpha = 1 # First phase of development of new major, minor or patch release
+    release_candidate = nil # Used before releasing new minor or major releases
 
-    version = "#{critical}.#{major}.#{minor}"
-
-    if patch
-      version += ".#{'%03d' % patch}"
-    elsif pre_release
-      version += "pre#{'%02d' % pre_release}"
+    "#{major}.#{minor}.#{tiny}" << if alpha
+      ".alpha#{alpha}"
     elsif release_candidate
-      version += "rc#{release_candidate}"
-    elsif alpha
-      version += "alpha#{'%02d' % alpha}"
-    elsif beta
-      version += "beta#{beta}"
+      ".rc#{release_candidate}"
     end
-    version
   end
 
-
-  VERSION = version
+  VERSION = version.freeze
 end
